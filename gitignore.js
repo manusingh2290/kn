aaefd42cb6a3c93e1cb58cd60e701c7d0ee5d321
Manendra Singh <singhdaleep8642@gmail.com>
2025-08-21 00:41:43 +0530

    Add files via upload

---
+++ gitignore.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyBnpQBeIIoJlKyQrGbP8M1sLMtXcqFYWyk",
+  authDomain: "kumple-e5d95.firebaseapp.com",
+  projectId: "kumple-e5d95",
+  storageBucket: "kumple-e5d95.firebasestorage.app",
+  messagingSenderId: "860172538391",
+  appId: "1:860172538391:web:c62cfc006cadad4dfa0252",
+  measurementId: "G-8NW5RX7LP1"
+    };
