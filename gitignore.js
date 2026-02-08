974706f64992571594853cb99d6e15820eb46fbd
Manendra Singh <singhdaleep8642@gmail.com>
2026-02-08 15:33:16 +0530

    Delete gitignore.js

--- gitignore.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyASXA-EXaZUUeOTAGdmIm-PcWdC-oo-YmI",
-  authDomain: "kumple-9334d.firebaseapp.com",
-  projectId: "kumple-9334d",
-  storageBucket: "kumple-9334d.firebasestorage.app",
-  messagingSenderId: "375859052152",
-  appId: "1:375859052152:web:0fb944402d15c0ffc9778d",
-  measurementId: "G-LS0JECZ8YC"
-};
